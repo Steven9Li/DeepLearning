7c539d9b1abf4593527b654cf9557504ca642cbb
unknown <steven>
2018-09-27 16:04:35 +0800

    ASR_Study

---
+++ ASR/Study_1.py
@@ -0,0 +1,8 @@
+import platform as plat
+import os
+import time
+
+from general_function.file_wav import *
+from general_function.file_dict import *
+from general_function.gen_func import *
+from readdata24 import DataSpeech
--- Keras/mlp.py
+++ Keras/mlp.py
@@ -9,6 +9,8 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import RMSprop
+from keras.models import load_model
+from keras import backend as K
 
 if(__name__=='__main__'):
 
@@ -35,15 +37,28 @@ if(__name__=='__main__'):
     model = Sequential()
     model.add(Dense(512, activation='relu',input_shape=(784,)))
     model.add(Dropout(0.2))
-    model.add(Dense(512,activation='relu'))
+    model.add(Dense(256,activation='relu'))
     model.add(Dropout(0.2))
     model.add(Dense(num_class,activation='softmax'))
-    model.summary()
+    #model.summary()
 
     model.compile(loss = 'categorical_crossentropy', optimizer = RMSprop(), metrics = ['accuracy'])
+    #json_string = model.to_json()
+    #print(json_string)
+    history = model.fit(train_data, train_labels, epochs = 4, verbose = 2, validation_data=(test_data, test_labels))
+    #model.save('mlp_model_1')
+
+    #old_model = load_model('mlp_model_1')
+    #get_3rd_layer_output = K.function([model.layers[0].input,K.learning_phase()],
+    #                              [model.layers[2].output])
+    #layer_output = get_3rd_layer_output([train_data,1])[0]
+    #print(len(layer_output[0]))
+    #history = model.fit(train_data, train_labels, epochs = 1, verbose = 1, validation_data=(test_data, test_labels))
+    '''
     history = model.fit(train_data, train_labels, epochs = epochs, verbose = 1, validation_data=(test_data, test_labels))
     score = model.evaluate(test_data,test_labels,verbose=0)
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
+    '''
 
 
